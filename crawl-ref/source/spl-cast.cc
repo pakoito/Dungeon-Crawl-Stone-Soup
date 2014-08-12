df36e7a2551a90b6a7ef8f87bf76cd260defaa6c
Nicholas Feinberg <pleasingfung@gmail.com>
2014-08-11 22:18:01 -0700

    Refactor spell uselessness code

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -890,64 +890,6 @@ static void _spellcasting_side_effects(spell_type spell, god_type god,
     alert_nearby_monsters();
 }
 
-static bool _vampire_cannot_cast(spell_type spell)
-{
-    if (you.species != SP_VAMPIRE)
-        return false;
-
-    if (you.hunger_state > HS_SATIATED)
-        return false;
-
-    // Satiated or less
-    switch (spell)
-    {
-    case SPELL_BEASTLY_APPENDAGE:
-    case SPELL_BLADE_HANDS:
-    case SPELL_DRAGON_FORM:
-    case SPELL_ICE_FORM:
-    case SPELL_SPIDER_FORM:
-    case SPELL_STATUE_FORM:
-    case SPELL_STONESKIN:
-        return true;
-    case SPELL_CURE_POISON:
-        // You can become poisoned at any state but bloodless; allow curing
-        // it under the same conditions.
-        return you.hunger_state <= HS_STARVING;
-    default:
-        return false;
-    }
-}
-
-#if TAG_MAJOR_VERSION == 34
-static bool _too_hot_to_cast(spell_type spell)
-{
-    if (you.species != SP_LAVA_ORC)
-        return false;
-
-    // Lava orcs can never benefit from casting stoneskin.
-    if (spell == SPELL_STONESKIN)
-        return true;
-
-    // Lava orcs have no restrictions if their skin is
-    // non-molten.
-    if (temperature_effect(LORC_STONESKIN))
-        return false;
-
-    // If it is, though, they lose out on these spells:
-    switch (spell)
-    {
-    case SPELL_STATUE_FORM: // Stony self is too melty
-    // Too hot for these ice spells:
-    case SPELL_ICE_FORM:
-    case SPELL_OZOCUBUS_ARMOUR:
-    case SPELL_CONDENSATION_SHIELD:
-        return true;
-    default:
-        return false;
-    }
-}
-#endif
-
 bool is_prevented_teleport(spell_type spell)
 {
     return (spell == SPELL_BLINK
@@ -958,39 +900,8 @@ bool is_prevented_teleport(spell_type spell)
 
 bool spell_is_uncastable(spell_type spell, string &msg)
 {
-    // XXX: refactor me
-    if (you.undead_state() == US_UNDEAD
-        && (spell == SPELL_BORGNJORS_REVIVIFICATION
-            || spell == SPELL_DEATHS_DOOR
-            || spell == SPELL_SUBLIMATION_OF_BLOOD
-            || spell == SPELL_INTOXICATE
-            || spell == SPELL_REGENERATION
-            || spell_is_form(spell) && spell != SPELL_NECROMUTATION))
-    {
-        msg = "You cannot cast that spell in your current form!";
-        return true;
-    }
-
-    if (_vampire_cannot_cast(spell))
-    {
-        msg = "Your current blood level is not sufficient to cast that spell.";
-        return true;
-    }
-
-#if TAG_MAJOR_VERSION == 34
-    if (_too_hot_to_cast(spell))
-    {
-        if (spell == SPELL_STONESKIN && temperature_effect(LORC_STONESKIN))
-            msg = "Your skin is already made of stone.";
-        else if (spell == SPELL_STONESKIN && !temperature_effect(LORC_STONESKIN))
-            msg = "Your skin is already made of molten stone.";
-        else
-            msg = "Your temperature is too high to benefit from that spell.";
-        return true;
-    }
-#endif
-
-    return false;
+    msg = spell_uselessness_reason(spell);
+    return msg != "";
 }
 
 #ifdef WIZARD
@@ -1071,15 +982,6 @@ static bool _spellcasting_aborted(spell_type spell,
     if (!wiz_cast && spell_is_uncastable(spell, msg))
     {
         mprf("%s", msg.c_str());
-        return true;
-    }
-
-    if (is_prevented_teleport(spell))
-    {
-        if (you.species == SP_FORMICID)
-            mpr("You cannot teleport.");
-        else
-            mpr("You cannot teleport right now.");
         return true;
     }
 
